ee5adb42078074a936000a9c6a512eccbdfaa101
JK <jas.kocurek@wp.pl>
2020-08-24 17:33:24 +0200

    db conn fixed

--- app.js
+++ app.js
@@ -10,9 +10,12 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth';
+const dbURI = 'mongodb+srv://admin:test12345@cluster0.de7xv.mongodb.net/auth?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(3000))
+  .then((result) => {
+    console.log('DB connection established');
+    app.listen(3000, () => {console.log("Lesitening for requests on port 3000")});
+  })
   .catch((err) => console.log(err));
 
 // routes
