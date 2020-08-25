a65066774bd650571dbe3948d262bcdf300328a1
JK <jas.kocurek@wp.pl>
2020-08-25 18:33:31 +0200

    cookies

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -23,3 +24,26 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
+
+//Cookies
+app.get('/set-cookies', (req, res) => {
+
+  //res.setHeader('Set-Cookie', 'newUser=true');
+
+  //name, valapp.get('/set-cookies', (req, res) => {
+
+  // 3rd param is options object
+  res.cookie('newUser', false, { maxAge: 1000 * 3600, httpOnly: true });
+
+  res.send('Cookie sent!');
+
+});
+
+app.get('/read-cookies', (req, res) => {
+
+  const cookies = req.cookies;
+  console.log(cookies);
+
+  res.json(cookies);
+
+});
