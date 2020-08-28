5a9a3c3104cee553ad4fa30603fe65ccf31250da
JK <jas.kocurek@wp.pl>
2020-08-28 22:18:47 +0200

    coditional rendering, tut done

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const secret = require('../secret');
+const User = require('../models/User');
 
 const requireAuth = (req, res, next) => {
     // using this middleware we can make it so any page can require the user to be authenticated
@@ -21,4 +22,28 @@ const requireAuth = (req, res, next) => {
     }
 }
 
-module.exports = { requireAuth }
+// check curr user
+const checkUser = (req, res, next) => {
+    const token = req.cookies.jwt;
+
+    if (token) {
+        jwt.verify(token, secret, async (err, decodedToken) => {
+            if (err) {
+                console.log(err.message);
+                res.locals.user = null;
+                next();
+            } else {
+                console.log(decodedToken);
+                let user = await User.findById(decodedToken.id);
+                // passing data into view from middleware
+                res.locals.user = user;
+                next();
+            }
+        });
+    } else {
+        res.locals.user = null;
+        next();
+    }
+}
+
+module.exports = { requireAuth, checkUser }
